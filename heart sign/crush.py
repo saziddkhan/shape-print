e43e97b629e59842b9b4a090a534a1f697bb672f
sazid <96243953+saziddkhan@users.noreply.github.com>
2022-03-31 23:28:56 +0530

    Add files via upload

---
+++ heart sign/crush.py
@@ -0,0 +1,14 @@
+from turtle import *
+import time
+color('pink')
+begin_fill()
+pensize(20)
+left(90)
+forward(133)
+circle(50, 200)
+right(140)
+circle(50, 200)
+forward(133)
+end_fill()
+
+time.sleep(8)
